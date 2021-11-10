a6309a93f9bb4fcd5a4ddfe50148ed73cf949e20
zhafiraak <zhafirakhafianti19@gmail.com>
2021-11-10 21:35:50 +0700

    py

---
+++ lat2.py
@@ -0,0 +1,7 @@
+print('Program mengurutkan data')
+
+pertama = int(input("Bilangan pertama: "))
+kedua = int(input("Bilangan kedua: "))
+ketiga = int(input("Bilangan ketiga: "))
+
+print(f'urutan: {pertama}, {kedua}, {ketiga}')
